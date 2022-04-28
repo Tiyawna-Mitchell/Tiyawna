68335dc06a86f8f8f54d873b5a2a2b66b53e8ce6
Tiyawna-Mitchell <104540892+Tiyawna-Mitchell@users.noreply.github.com>
2022-04-27 20:43:29 -0500

    Add files via upload

---
+++ Mitchell-Tiyawna-PythonCode-ITT103-S2022.py
@@ -0,0 +1,55 @@
+
+#  Input class
+#  Input sales
+#  Initalise commission
+#  Initalise rate
+#   IF class=0
+#   Stop Loop
+#   IF class=1
+#   IF sales <=$1000
+#       Set rate 6
+#   IF sales >=$1000 and <$2000
+#       Set rate 7
+#   Else
+#       Set rate 10
+#   IF class=2
+#   IF slaes <=$1000
+#       Set rate 4
+#   Else
+#       Set rate 6
+#   IF class=3
+#       Set rate 4.5
+#   ELSE
+#       Give Error!
+#   Claculate Commission
+#   Print Commission
+
+
+ #Print('Print 0 in class for exit')
+#While;
+ #class=int(input('class: '))
+# sales=int(input('sales: '))
+ #commission=0
+# rate=0
+ #If class==0:
+ #    print('Programm Stopped')
+    # elif class==1:
+         #IF sales<=$1000;
+#        rate=6
+#         elif sales >$1000 and sales <$2000
+ #        rate=7
+ #        else:rate 10
+  #       elif class==2:
+#             IF sales<$1000:
+ #                rate=4
+ #                else:
+ #               rate=6
+#                eliif class==3:
+#                    rate=4.5
+#                    else:
+ #                       Error!(Invalid class value.Class value shoould be either 1,2 or 3)
+                        
+#                        commission=sales*rate/100
+#                        print(f'commission = {commsission}')
+
+ 
